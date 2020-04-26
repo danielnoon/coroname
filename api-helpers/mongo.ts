a1ef948df36df9ee96ebcf300e5badaca3a8b8a6
Daniel Noon <supercookies@outlook.com>
2020-04-26 01:28:13 -0500

    log mongoose

--- api-helpers/mongo.ts
+++ api-helpers/mongo.ts
@@ -1,5 +1,9 @@
 import * as mongoose from 'mongoose';
 
-export const connect = () => mongoose.connect(process.env.DB_URI || 'mongodb://localhost:27017/coroname', {
-    useNewUrlParser: true, useUnifiedTopology: true
-});
+export const connect = () => {
+  console.log(mongoose);
+  mongoose.connect(process.env.DB_URI || 'mongodb://localhost:27017/coroname', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
